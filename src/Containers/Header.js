006fc24ce4b9ce4c28709b27320385cb8cc9a347
Yogesh <yogeshzade999@gmail.com>
2022-09-18 01:43:02 +0530

    Change store name

--- src/Containers/Header.js
+++ src/Containers/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='ui fixed menu'>
         <div className='ui container center'>
-            <h2>Fake Shop</h2>
+            <h2>Product Store</h2>
         </div>
     </div>
   )
