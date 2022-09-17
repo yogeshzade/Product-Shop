cf3aaf7f0d7fb9f50f56d34f0247e89f8c14a350
Yogesh <yogeshzade999@gmail.com>
2022-09-18 01:25:13 +0530

    Create Application of Product Shop using react redux and use css semantic ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Containers/Header';
+import ProductDetail from './Containers/ProductDetail';
+import ProductListing from './Containers/ProductListing';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
+    <Routes>
+      <Route path='/' element={<ProductListing />} />
+      <Route path='/product/:productId' element={<ProductDetail/>} />
+      <Route>404 Not Found</Route>
+    </Routes>
     </div>
   );
 }
